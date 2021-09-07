815b09c2dc4e2f20e91a18ab82b4506e44e2036e
breezeJACK <wiz123@sina.com>
2021-09-07 22:28:59 +0800

    feat：对掉帧现象进行监控

--- src/test/test.vue
+++ src/test/test.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="test">
-    test
+    <div v-for="(a,index) in list" :key="index">
+        {{a}}
+    </div>
   </div>
 </template>
 
@@ -8,7 +10,16 @@
 
 
 export default {
-  name: "test"
+  name: "test",
+  data(){
+      return{list: []}
+  },
+  created(){
+      for(let i=1;i<1000;i++){
+           this.list.push(1111)
+      }
+     
+  }
 };
 </script>
 
